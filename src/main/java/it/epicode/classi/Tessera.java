f78116b649ed47504790b5b04bb80c6fb5a5c0ac
“Piero793” <pierolacitignola7@gmail.com>
2025-03-31 11:41:09 +0200

    classe utente+tessera

--- src/main/java/it/epicode/classi/Tessera.java
+++ src/main/java/it/epicode/classi/Tessera.java
@@ -2,11 +2,13 @@ package it.epicode.classi;
 
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 
 @Data
+@NoArgsConstructor
 public class Tessera  {
 
     @Id
@@ -18,6 +20,10 @@ public class Tessera  {
         this.numeroTessera = numeroTessera;
         this.dataDiScadenza = dataDiEmissione.plusYears(1);
         this.dataDiEmissione = dataDiEmissione;
+    }
+
+    public boolean isValid() {
+        return LocalDate.now().isBefore(dataDiScadenza);  //metodo per vedere se la tessera è valida
     }
 
 
