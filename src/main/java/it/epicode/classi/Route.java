c2860fc4e473f849e955a9e5d4106a8db0e7a6aa
Luca Zuin <zuin.luca1111@gmail.com>
2025-03-31 12:11:05 +0200

    setter, getter, constructor

--- src/main/java/it/epicode/classi/Route.java
+++ src/main/java/it/epicode/classi/Route.java
@@ -1,15 +1,34 @@
 package it.epicode.classi;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Route {
 
+    @Getter
     @Id
     private Long id;
 
+    @Setter
+    @Getter
     private String departureArea;
+
+    @Setter
+    @Getter
     private String arrivalArea;
+
+    @Setter
+    @Getter
     private int expectedTime;
 
+    public Route() {}
+
+    public Route(Long id, String departureArea, String arrivalArea, int expectedTime) {
+        this.id = id;
+        this.departureArea = departureArea;
+        this.arrivalArea = arrivalArea;
+        this.expectedTime = expectedTime;
+    }
 
 }
