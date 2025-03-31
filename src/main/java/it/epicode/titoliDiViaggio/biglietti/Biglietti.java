13d2a96d1484adca369ea02c37e0689a9b98b25f
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-03-31 12:34:10 +0200

    classe biglietti speriamo

--- src/main/java/it/epicode/titoliDiViaggio/biglietti/Biglietti.java
+++ src/main/java/it/epicode/titoliDiViaggio/biglietti/Biglietti.java
@@ -2,6 +2,17 @@ package it.epicode.titoliDiViaggio.biglietti;
 
 import it.epicode.titoliDiViaggio.TitoloDiViaggio;
 
+import java.time.LocalDate;
+
+
 public class Biglietti extends TitoloDiViaggio {
 
+    private boolean vidimato;
+    private LocalDate dataEmissione;
+
+    public Biglietti(Long codiceUnivoco, LocalDate dataEmissione, boolean vidimato) {
+        super(codiceUnivoco);
+        this.dataEmissione = dataEmissione;
+        this.vidimato = vidimato;
+    }
 }
