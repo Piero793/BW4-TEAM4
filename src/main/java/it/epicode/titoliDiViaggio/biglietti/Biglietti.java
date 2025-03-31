63853c05656b5274c816e3db92a29fdb844bb2d6
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-03-31 12:32:09 +0200

    classe biglietti

--- src/main/java/it/epicode/titoliDiViaggio/biglietti/Biglietti.java
+++ src/main/java/it/epicode/titoliDiViaggio/biglietti/Biglietti.java
@@ -3,7 +3,5 @@ package it.epicode.titoliDiViaggio.biglietti;
 import it.epicode.titoliDiViaggio.TitoloDiViaggio;
 
 public class Biglietti extends TitoloDiViaggio {
-    public Biglietti(Long codiceUnivoco) {
-        super(codiceUnivoco);
-    }
+
 }
